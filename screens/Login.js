a56d109b4b17b0e964291680b4e3e6cc3f99c5b1
Emil Laudon <emillaudon@gmail.com>
2021-02-05 00:49:31 +0100

    eslint fixes

--- screens/Login.js
+++ screens/Login.js
@@ -1,121 +1,135 @@
-
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useState, useRef, useEffect } from 'react';
 import { auth } from '../firebase';
 
-const logo = require('../assets/logo.png')
+const logo = require('../assets/logo.png');
 
 import {
   StyleSheet,
   Image,
   Text,
   View,
-  FlatList,
   TouchableOpacity,
-  TextInput
-} from "react-native";
+  TextInput,
+} from 'react-native';
 
 const login = async (email, password, setErrorText, navigation) => {
-    try {
-        await auth.signInWithEmailAndPassword(email, password);
-        navigation.navigate('Home')
-    } catch (error) {
-        let e = error.toString();
-        console.log(e)
-        setErrorText(e)
-    }
-}
+  try {
+    await auth.signInWithEmailAndPassword(email, password);
+    navigation.navigate('Home');
+  } catch (error) {
+    let e = error.toString();
+    console.log(e);
+    setErrorText(e);
+  }
+};
 
 const register = async (email, password, setErrorText, navigation) => {
-    try {
-        await auth.createUserWithEmailAndPassword(email, password);
-        navigation.navigate('Home');
-    } catch (error) {
-        let e = error.toString();
-        setErrorText(e)
-    }
-}
+  try {
+    await auth.createUserWithEmailAndPassword(email, password);
+    navigation.navigate('Home');
+  } catch (error) {
+    let e = error.toString();
+    setErrorText(e);
+  }
+};
 
 const LoginBox = ({ navigation }) => {
-    const [errorText, setErrorText] = useState('')
+  const [errorText, setErrorText] = useState('');
 
-    let email = '';
-    let password = '';
+  const textInputRef = useRef(null);
 
-    return (
-        <View style={styles.login}>
-            <TextInput 
-                placeholder={'E-mail'}
-                style={ styles.inputText }
-                onChangeText={(text) => email = text}
-            />
+  const toggleFocus = () => {
+    if (textInputRef.current.isFocused()) {
+      textInputRef.current.blur();
+    } else {
+      textInputRef.current.focus();
+    }
+  };
 
-            <TextInput 
-                placeholder={'Password'}
-                secureTextEntry={true}
-                style={ styles.inputText }
-                onChangeText={(text) => password = text}
-            />
+  let email = '';
+  let password = '';
 
-            <View style={{ flexDirection: 'row' }}>
-                <TouchableOpacity 
-                    onPress={() => login(email, password, setErrorText, navigation)}
-                    style={styles.loginButton}>
-                    <Text>Login</Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                    style={styles.loginButton}
-                    onPress={() => register(email, password, setErrorText, navigation)}>
-                    <Text>Register</Text>
-                </TouchableOpacity>
-            </View>
+  useEffect(() => {
+    toggleFocus();
+  });
 
-            <Text style={{ fontSize: 10,color: 'red', alignSelf: 'center', margin: 15}}>
-                {errorText}
-            </Text>
-        </View>
-    );
-}
+  return (
+    <View style={styles.login}>
+      <TextInput
+        ref={textInputRef}
+        placeholder={'E-mail'}
+        style={styles.inputText}
+        onChangeText={(text) => (email = text)}
+      />
 
+      <TextInput
+        placeholder={'Password'}
+        secureTextEntry={true}
+        style={styles.inputText}
+        onChangeText={(text) => (password = text)}
+      />
+
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity
+          onPress={() => login(email, password, setErrorText, navigation)}
+          style={styles.loginButton}
+        >
+          <Text>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => register(email, password, setErrorText, navigation)}
+        >
+          <Text>Register</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text
+        style={{ fontSize: 10, color: 'red', alignSelf: 'center', margin: 15 }}
+      >
+        {errorText}
+      </Text>
+    </View>
+  );
+};
 
 export default function Login({ navigation }) {
-    console.log(navigation)
+  console.log(navigation);
   return (
     <View style={styles.loginScreen}>
-        <Image style={{ height: 200, width: 300, marginBottom: 50 }}source={logo}/>
-        <LoginBox navigation={ navigation } />
-
+      <Image
+        style={{ height: 200, width: 300, marginBottom: 30 }}
+        source={logo}
+      />
+      <LoginBox navigation={navigation} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   loginScreen: {
-      flexDirection: 'column',
-      justifyContent: 'flex-start',
-      alignItems: 'center',
-      height: '100%',
-      width: '100%',
-      backgroundColor: '#E50D7B',
-      paddingTop: '30%'
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#E50D7B',
+    paddingTop: '30%',
   },
   inputText: {
-      backgroundColor: 'white',
-      padding: 10,
-      borderRadius: 5,
-      marginBottom: 20,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
   },
   loginButton: {
-      alignSelf: 'center',
-      backgroundColor: 'white',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 10,
-      marginHorizontal: 10,
-      padding: 10,
-      width: 100
-  }
-
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    marginHorizontal: 10,
+    padding: 10,
+    width: 100,
+  },
 });
